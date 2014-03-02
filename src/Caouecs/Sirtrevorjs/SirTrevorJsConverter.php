551577aa724dce81f6df9652f6a0be63925dbbf0
caouecs <caouecs@caouecs.net>
2014-03-02 01:37:22 +0100

    Fix on embedded tweets

--- src/Caouecs/Sirtrevorjs/SirTrevorJsConverter.php
+++ src/Caouecs/Sirtrevorjs/SirTrevorJsConverter.php
@@ -70,7 +70,7 @@ class SirTrevorJsConverter
      */
     public function twitterToHtml($data)
     {
-        return '<blockquote class="twitter-tweet" align="center">
+        return '<blockquote class="twitter-tweet tw-align-center">
             <p>'.$data['text'].'</p>
             &mdash; '.$data['user']['name'].' (@'.$data['user']['screen_name'].')
             <a href="'.$data['status_url'].'" data-datetime="'.$data['created_at'].'">'.$data['created_at'].'</a>
