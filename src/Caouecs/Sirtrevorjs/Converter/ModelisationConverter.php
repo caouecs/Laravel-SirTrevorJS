b7e97cbb0428724dc3160bd83a5dd0ae7725ebea
Fred Delrieu <caouecs@caouecs.net>
2014-12-01 22:39:29 +0000

    Modelisation by Sketchfab

---
+++ src/Caouecs/Sirtrevorjs/Converter/ModelisationConverter.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Laravel4-SirTrevorJs
+ *
+ * @link https://github.com/caouecs/Laravel4-SirTrevorJS
+ */
+
+namespace Caouecs\Sirtrevorjs\Converter;
+
+/**
+ * Modelisation for Sir Trevor Js
+ *
+ * @package Caouecs\Sirtrevorjs\Converter
+ */
+class ModelisationConverter
+{
+    /**
+     * List of types for sound
+     *
+     * @access protected
+     * @var array
+     */
+    protected $types = array(
+        "sketchfab"
+    );
+
+    /**
+     * Sketchfab block
+     *
+     * @access public
+     * @return string
+     */
+    public function sketchfabToHtml()
+    {
+        return $this->view("modelisation.sketchfab", array(
+            "remote" => $this->data['remote_id']
+        ));
+    }
+}
---
+++ src/views/modelisation/sketchfab.blade.php
@@ -0,0 +1,4 @@
+<p class="st-sketchlab">
+  <iframe allowFullScreen webkitallowfullscreen mozallowfullscreenscrolling="no"
+    src="https://sketchfab.com/models/{{ $remote_id }}/embed" width="640" height="480" frameborder="0"></iframe>
+</p>
