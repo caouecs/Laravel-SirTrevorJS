c6c3328ef32e4e30e676fc477f8d275532ec0b57
Fred Delrieu <caouecs@caouecs.net>
2015-06-12 15:55:46 +0000

    fix: #18 - no escape

--- src/views/js.blade.php
+++ src/views/js.blade.php
@@ -2,18 +2,18 @@
 $(function(){
 
 @if ($config['language'] != "en")
-  SirTrevor.LANGUAGE = '{{ $config['language']}}';
+  SirTrevor.LANGUAGE = '{!! $config['language'] !!}';
 @endif
 
-  SirTrevor.setDefaults({uploadUrl: '{{ $config['uploadUrl'] }}'});
+  SirTrevor.setDefaults({uploadUrl: '{!! $config['uploadUrl'] !!}'});
   SirTrevor.setBlockOptions('Tweet', {
     fetchUrl: function(tweetID) {
-        return '{{ $config['tweetUrl'] }}?tweet_id=' + tweetID;
+        return '{!! $config['tweetUrl'] !!}?tweet_id=' + tweetID;
     }
   });
   window.editor = new SirTrevor.Editor({
-    el: $('.{{ $config['class'] }}'),
-    blockTypes: [{{ $config['blocktypes'] }}]
+    el: $('.{!! $config['class'] !!}'),
+    blockTypes: [{!! $config['blocktypes'] !!}]
   });
 });
 </script>
