a9ec73acce80be45922d3578f23dc7dd2855757a
caouecs <caouecs@caouecs.net>
2016-05-23 08:01:10 +0200

    fix: tweet - fb - not op-social

--- src/views/fb/social/tweet.blade.php
+++ src/views/fb/social/tweet.blade.php
@@ -1,9 +1,7 @@
-<figure class="op-social">
-  <iframe>
-    <p>{!! $data['text'] !!}</p>
-    &mdash; {!! $data['user']['name'] !!} (&#64;{!! $data['user']['screen_name'] !!})
-    <a href="{!! $data['status_url'] !!}" data-datetime="{!! $data['created_at'] !!}">
-      {!! $data['created_at'] !!}
-    </a>
-  </iframe>
-</figure>
+<iframe>
+  <p>{!! $data['text'] !!}</p>
+  &mdash; {!! $data['user']['name'] !!} (&#64;{!! $data['user']['screen_name'] !!})
+  <a href="{!! $data['status_url'] !!}" data-datetime="{!! $data['created_at'] !!}">
+    {!! $data['created_at'] !!}
+  </a>
+</iframe>
