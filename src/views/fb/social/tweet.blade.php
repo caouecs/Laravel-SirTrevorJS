b5c4d0d4441d20a59e56d5ce54f6eb175789f1b7
caouecs <caouecs@caouecs.net>
2016-08-06 23:41:11 +0200

    fix: fb views - twitter

--- src/views/fb/social/tweet.blade.php
+++ src/views/fb/social/tweet.blade.php
@@ -1,7 +1,9 @@
-<iframe>
-  <p>{!! $data['text'] !!}</p>
-  &mdash; {!! $data['user']['name'] !!} (&#64;{!! $data['user']['screen_name'] !!})
-  <a href="{!! $data['status_url'] !!}" data-datetime="{!! $data['created_at'] !!}">
-    {!! $data['created_at'] !!}
-  </a>
-</iframe>
+<figure class="op-interactive">
+  <iframe>
+    <p>{!! $data['text'] !!}</p>
+    &mdash; {!! $data['user']['name'] !!} (&#64;{!! $data['user']['screen_name'] !!})
+    <a href="{!! $data['status_url'] !!}" data-datetime="{!! $data['created_at'] !!}">
+      {!! $data['created_at'] !!}
+    </a>
+  </iframe>
+</figure>
