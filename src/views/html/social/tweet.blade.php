23c5f7f16beb7af54bec5361972f781e3d8a43ce
caouecs <caouecs@caouecs.net>
2016-10-02 21:28:37 +0200

    feature: social - tweet template

--- src/views/html/social/tweet.blade.php
+++ src/views/html/social/tweet.blade.php
@@ -1,6 +1,6 @@
-<blockquote class="twitter-tweet tw-align-center">
+<blockquote class="twitter-tweet tw-align-center" data-lang="fr">
 @if (!empty($data['text']))
-  <p>{!! $data['text'] !!}</p>
+  <p lang="en" dir="ltr">{!! $data['text'] !!}</p>
   &mdash; {!! $data['user']['name'] !!} (&#64;{!! $data['user']['screen_name'] !!})
 @endif
   <a href="{!! $data['status_url'] !!}" data-datetime="{!! $data['created_at'] !!}">
