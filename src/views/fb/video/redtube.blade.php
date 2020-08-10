a3a4f01fb72a47e8cfd2454843342c34e29fc96a
Fred Delrieu <caouecs@users.noreply.github.com>
2020-08-10 13:59:13 +0200

    feature: title for redtube

--- src/views/fb/video/redtube.blade.php
+++ src/views/fb/video/redtube.blade.php
@@ -1,6 +1,5 @@
 @extends('sirtrevorjs::html.video.base')
 
 @section('video')
-  <iframe loading="lazy" src="https://embed.redtube.com/?id={!! $remote !!}&amp;bgcolor=000000" width="434" height="344"
- ></iframe>
+  <iframe loading="lazy" src="https://embed.redtube.com/?id={!! $remote !!}&amp;bgcolor=000000" width="434" height="344" title="{!! $title !!}></iframe>
 @stop
