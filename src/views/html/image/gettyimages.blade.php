7f10a42e81a64fba685c8e3f1cdb17bcba7e9065
Fred Delrieu <caouecs@caouecs.net>
2023-06-02 17:16:08 +0200

    fix: gettyimages if not title

--- src/views/html/image/gettyimages.blade.php
+++ src/views/html/image/gettyimages.blade.php
@@ -1,5 +1,5 @@
 <p class="st-gettyimages">
   <iframe loading="lazy" src="https://embed.gettyimages.com/embed/{!! $remote_id !!}" width="{!! $width !!}" height="{!! $height !!}"
-    title="{!! $title !!}">
+    title="{!! $title ?? '' !!}">
   </iframe>
 </p>
